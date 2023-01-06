e7bb12b6a605e0989a7c59b177183095cd240653
connorAlex <alex.connor.cochrane@gmail.com>
2023-01-06 15:35:01 -0600

    add site docs

---
+++ script.js
@@ -0,0 +1,14 @@
+const openLink = (e) => {
+    if (e.target.tagName === "A") {
+        console.log("link clicked");
+        e.stopPropagation()
+        return;
+    };
+    const link = e.currentTarget.dataset.link;
+    window.open(link);
+}
+
+const nodes = document.querySelectorAll("section");
+nodes.forEach((item) => {
+    item.addEventListener("click", openLink);
+})
