c2269b49c5dfc37bb33cd071854d681d0c6dcc2a
connorAlex <alex.connor.cochrane@gmail.com>
2023-01-09 09:05:11 -0600

    prettier format

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const openLink = (e) => {
-    if (e.target.tagName === "A") {
-        console.log("link clicked");
-        e.stopPropagation()
-        return;
-    };
-    const link = e.currentTarget.dataset.link;
-    window.open(link);
-}
+  if (e.target.tagName === "A") {
+    console.log("link clicked");
+    e.stopPropagation();
+    return;
+  }
+  const link = e.currentTarget.dataset.link;
+  window.open(link);
+};
 
 const nodes = document.querySelectorAll("section");
 nodes.forEach((item) => {
-    item.addEventListener("click", openLink);
-})
+  item.addEventListener("click", openLink);
+});
